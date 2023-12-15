2c5ef49e3979c036e900fc5fbe8e0deb7c46e641
Ravan Mammadov <131792522+RavanM2003@users.noreply.github.com>
2023-12-15 22:39:58 +0400

    feat:Updated Product Detail,Added Category,Product

---
+++ BackEnd/Final Project/Final Project/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿using Final_Project.Models.Entity;
+
+namespace Final_Project.Models
+{
+    public class Category : BaseEntity
+    {
+        public string ImageUrl { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ BackEnd/Final Project/Final Project/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using Final_Project.Models.Entity;
+
+namespace Final_Project.Models
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public double? SalePrice { get; set; }
+        public string ProductCode { get; set; }
+        public string Color { get; set; }
+        public string HowToUse { get; set; }
+        public int StockCount { get; set; }
+        public double Volume { get; set; }
+    }
+}

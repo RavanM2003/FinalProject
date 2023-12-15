b52c52ad57123ae11d01ca155b3dbcaa3dea4131
Ravan Mammadov <131792522+RavanM2003@users.noreply.github.com>
2023-12-15 13:18:04 +0400

    feat: "Made ViewComponents for Header And Footer"

--- BackEnd/Final Project/Final Project/ViewComponents/FooterViewComponent.cs
+++ BackEnd/Final Project/Final Project/ViewComponents/FooterViewComponent.cs
@@ -1,23 +1,24 @@
 ﻿using Final_Project.DAL;
+using Final_Project.ViewModels.Footer;
 using Final_Project.ViewModels.Header;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace Final_Project.ViewComponents
 {
-    public class HeaderViewComponent : ViewComponent
+    public class FooterViewComponent : ViewComponent
     {
         private readonly AppDbContext _context;
 
-        public HeaderViewComponent(AppDbContext context)
+        public FooterViewComponent(AppDbContext context)
         {
             _context = context;
         }
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            HeaderVM headerVM = new();
-            headerVM.Setting = _context.Settings.Where(s=>!s.IsDeleted).AsNoTracking().ToDictionary(s=>s.Key, s=>s.Value);
-            return View(headerVM);
+            FooterVM footerVM = new();
+            footerVM.Setting = _context.Settings.Where(s => !s.IsDeleted).AsNoTracking().ToDictionary(s => s.Key, s => s.Value);
+            return View(footerVM);
         }
     }
 }
---
+++ BackEnd/Final Project/Final Project/ViewModels/Footer/FooterVM.cs
@@ -0,0 +1,7 @@
+﻿namespace Final_Project.ViewModels.Footer
+{
+    public class FooterVM
+    {
+        public Dictionary<string, string> Setting { get; set; }
+    }
+}
